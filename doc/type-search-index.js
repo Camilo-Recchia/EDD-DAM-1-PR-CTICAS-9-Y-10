6a7c7338caf48c25e5a5a082ca74fec13507367a
Camilo <camilitovillarroel@gmail.com>
2026-05-26 21:09:09 +0200

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"ejemplo","l":"Rectangle"},{"p":"ejemplo","l":"Rectangulo_Test"}];updateSearchResults();
